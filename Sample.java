eebd51f5ed9ac376a335f32b4acdcbb27121748d
9318076 <9318076@wcpschools.wcpss.local>
2017-05-10 11:34:17 -0400

    push from sublime text

---
+++ Sample.java
@@ -0,0 +1,35 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Sample extends JFrame {
+	private final JButton b = new JButton();
+
+	public Sample() {
+		super();
+		this.setTitle("HelloApp");
+		this.getContentPane().setLayout(null);
+		this.setBounds(100, 100, 180, 140);
+		this.add(makeButton());
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	private JButton makeButton() {
+		b.setText("Click me!");
+		b.setBounds(40, 40, 100, 30);
+		b.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(b, "Hello World!");
+			}
+		});
+		return b;
+	}
+
+	public static void main(String[] args) {
+		new Sample();
+    }
+}
