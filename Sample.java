f101dc28f690ab0b3667b3838d01dfabec842d98
Joel <howell.joel.t@gmail.com>
2017-05-11 10:34:14 -0400

    Changed

--- Sample.java
+++ Sample.java
@@ -10,7 +10,7 @@ public class Sample extends JFrame {
 
 	public Sample() {
 		super();
-		this.setTitle("HellISISopp");
+		this.setTitle("HarambeSopp");
 		this.getContentPane().setLayout(null);
 		this.setBounds(100, 100, 180, 140);
 		this.add(makeButton());
