47a81aef5eb7eb26da5389ba25cdcf65bb220c6f
Pavan <dayalpavan@gmail.com>
2017-05-22 08:44:54 -0400

    removed outdated Swing audio utility

--- src/utils/SoundUtils.java
+++ src/utils/SoundUtils.java
@@ -2,43 +2,12 @@
 
 package utils;
 
-import javax.sound.sampled.Clip;
 import javafx.scene.media.AudioClip;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
 import java.io.IOException;
 
 public class SoundUtils
 {
-
-	public static Clip getClip(String path)
-	{
-		Clip c = null;
-
-		try
-		{
-			c = AudioSystem.getClip();
-			AudioInputStream in = AudioSystem.getAudioInputStream(new File(path));
-			c.open(in);
-		}
-		catch (IOException e)
-		{
-			System.out.println("Error: couldn't find file '" + path + "'");
-		}
-		catch (UnsupportedAudioFileException e)
-		{
-			System.out.println("Error: file type not recognized\n"+e);
-		}
-		catch (LineUnavailableException e)
-		{
-			System.out.println("Error: couldn't play audio file");
-		}
-
-		return c;
-	}
 
 	public static AudioClip getAudioClip(String path)
 	{
