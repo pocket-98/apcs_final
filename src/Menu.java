5e9f9e7359205bef718182087d38d938282d8b6b
Joel <howell.joel.t@gmail.com>
2017-05-22 11:47:06 -0400

    menu panels

--- src/Menu.java
+++ src/Menu.java
@@ -2,9 +2,10 @@
 
 import javax.swing.SwingConstants;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import javafx.scene.media.AudioClip;
@@ -15,6 +16,7 @@ import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.GridLayout;
 import window.ClosableWindow;
 import window.ResizableComponent;
 import utils.ImageUtils;
@@ -24,15 +26,11 @@ import utils.GameUtils;
 public class Menu extends JFrame implements ClosableWindow.Listener, ResizableComponent.Listener
 {
 	// Frame Constants
-	private int width;
-	private int height;
 	private String title = "Ad Blocker The Game";
 	private String subtitle = "Main Menu";
 
 	// GUI Items
-	private JLabel youtubeLabel;
-	private JLabel titleLabel;
-	private JLabel subtitleLabel;
+
 	private JLabel backgroundLabel;
 
 	// Sound Items
@@ -53,12 +51,15 @@ public class Menu extends JFrame implements ClosableWindow.Listener, ResizableCo
 		addComponentListener(rs);
 		
 		System.out.println("Opening Window");
-		add(makeTitleLabel());
-		add(makeSubtitleLabel());
-		add(makeYoutubeLabel());
-		add(makeBackgroundLabel());
+		add(makeJPanel());
+		
+		add(makeTitlePanel());
 
+
+		add(makeBackgroundLabel());
 		playBackgroundMusic();
+
+
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH); 
@@ -66,12 +67,57 @@ public class Menu extends JFrame implements ClosableWindow.Listener, ResizableCo
 		setVisible(true);
 	}
 
+	private JPanel makeJPanel()
+	{
+		int w = 682, h = 480;
+		JPanel menu = new JPanel(new GridLayout(4, 1));
+		menu.setBounds((width - w) / 2, 500, w, h);
+		menu.setBackground(new Color(0, 0, 0, 0));
+		menu.add(makeNewGameButton(w, h));
+		return menu;
+	}
+
+	private JPanel makeTitlePanel()
+	{
+		int w = 682, h = 480;
+		JPanel title = new JPanel(null);
+		title.setBounds((width - w) / 2, 500, w, h);
+		title.setBackground(new Color(0, 0, 0, 0));
+		title.add(makeTitleLabel(w, h));
+		title.add(makeSubtitleLabel(w, h));
+		title.add(makeYoutubeLabel(w, h));
+		return title;
+	}
+
+	private JButton makeNewGameButton(int w, int h)
+	{
+		JButton newGamebtn = new JButton("NEW GAME");
+		newGamebtn.setIcon(ImageUtils.getImageIcon("res/menu/youtube.png", w, h/4));
+		
+
+		//newGamebtn.setBackground(new Color(0, 0, 0, 0));
+		newGamebtn.setBorderPainted(false);
+		newGamebtn.setFocusPainted(false);
+		newGamebtn.setContentAreaFilled(false);
+
+		newGamebtn.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				System.out.println("New game clicked");
+			}
+		});
+		return newGamebtn;
+	}
+
+
+
 	private JLabel makeTitleLabel()
 	{
 		int w = 500, h = 30;
 		Font techno = GameUtils.getFont(Font.BOLD, 30);
 		Color white = Color.WHITE;
-		titleLabel = new JLabel(title);
+		JLabel titleLabel = new JLabel(title);
 		titleLabel.setBounds((width-w)/2, 100, w, h);
 		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		titleLabel.setForeground(white);
@@ -79,12 +125,12 @@ public class Menu extends JFrame implements ClosableWindow.Listener, ResizableCo
 		return titleLabel;
 	}
 
-	private JLabel makeSubtitleLabel()
+	private JLabel makeSubtitleLabel(int w, int h)
 	{
-		int w = 300, h = 30;
+		//int w = 300, h = 30;
 		Font techno = GameUtils.getFont(Font.BOLD, 24);
 		Color white = Color.WHITE;
-		subtitleLabel = new JLabel(subtitle);
+		JLabel subtitleLabel = new JLabel(subtitle);
 		subtitleLabel.setBounds((width-w)/2, 130, w, h);
 		subtitleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		subtitleLabel.setForeground(white);
@@ -92,10 +138,9 @@ public class Menu extends JFrame implements ClosableWindow.Listener, ResizableCo
 		return subtitleLabel;
 	}
 
-	private JLabel makeYoutubeLabel() 
+	private JLabel makeYoutubeLabel(int w, int h) 
 	{
-		int w = 682, h = 480;
-		youtubeLabel = new JLabel();
+		JLabel youtubeLabel = new JLabel();
 		youtubeLabel.setBounds((width-w)/2, 50, w, h);
 		youtubeLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		youtubeLabel.setIcon(ImageUtils.getImageIcon("res/menu/youtube.png", w, h));
